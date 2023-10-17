091c5e2a54dda292c26a335e02e6ff05ab63785c
rightone0130 <rightone0130@gmail.com>
2023-10-17 10:14:31 -0700

    working on attends section

--- cool_entertainment/src/componenets/eventAttenders/eventAttenders.js
+++ cool_entertainment/src/componenets/eventAttenders/eventAttenders.js
@@ -60,6 +60,154 @@ const EventAttenders = () => {
 
 
 
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'3em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547541/Cool%20entertainment/IMG-20231014-WA0009-removebg-preview_ia9212.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Phamous Emmanuel </div>
+                        <div className="details">Anchor </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'3em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547541/Cool%20entertainment/IMG-20231014-WA0010-removebg-preview_mtd3td.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Ifeoluwa</div>
+                        <div className="details">Speaker <br/><small>[Fashion Design] </small> </div>
+                     </div>
+                </div>
+            </div>
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'3em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547540/Cool%20entertainment/IMG-20231014-WA0011-removebg-preview_qa6qkn.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Elebute Mario</div>
+                        <div className="details">Speaker <br/><small>[Mobile Phone Photography] </small> </div>
+                     </div>
+                </div>
+            </div>
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'2em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697587027/Cool%20entertainment/DSC_2542-removebgnewcropped2_euleuu.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Right-One R. Ayilara</div>
+                        <div className="details">Speaker <br/><small>[Web Design & Development] </small> </div>
+                     </div>
+                </div>
+            </div>
+
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'4em' }} className="card__image" width={260} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547540/Cool%20entertainment/IMG-20231016-WA0027-removebg-preview_n9mzrs.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Olamigoke Majorisaac Ajayi</div>
+                        <div className="details">Speaker <br/><small>[Health & Wellness] </small> </div>
+                     </div>
+                </div>
+            </div>
+
+
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'6em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547542/Cool%20entertainment/DSC_18601-removebg-preview_jjodbu.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Olubusola Ayilara</div>
+                        <div className="details">Verified </div>
+                     </div>
+                </div>
+            </div>
+
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'6em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547541/Cool%20entertainment/IMG-20231015-WA01271-removebg-preview_srvmoh.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Mosunmade Oyewo</div>
+                        <div className="details">Verified </div>
+                     </div>
+                </div>
+            </div>
+
+
+
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'6em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547540/Cool%20entertainment/DSC_9017xx1-removebg-preview_oo3wmb.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Racheal O. Ayilara</div>
+                        <div className="details">Verified </div>
+                     </div>
+                </div>
+            </div>
+
+
+            <div className="cards">
+                <div className="image__container">
+
+                    <img style={{zIndex: 1, marginTop:'6em' }} className="card__image" width={300} src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1697547541/Cool%20entertainment/DSC_5938aa1-removebg-preview_adsru7.png"/>
+                </div>
+
+                <div className="card__detailsContainer">
+                    <div className="card__details">
+                        <div className="name">Olasehinde Dorcas</div>
+                        <div className="details">Verified </div>
+                     </div>
+                </div>
+            </div>
+
+
+
+
+            
+            
+
+
+
 
 
 
